8060fb0836fff13b308d4db9c2bd2edbb69bba4c
unknown <LeeF@WKS-1260.intergen.org.nz>
2015-04-20 15:51:30 +1200

    change project structure.

--- HelloWorld/HelloWorld/AppDelegate.cs
+++ HelloWorld/HelloWorld/AppDelegate.cs
@@ -7,36 +7,53 @@ using UIKit;
 
 namespace HelloWorld
 {
-	// The UIApplicationDelegate for the application. This class is responsible for launching the
-	// User Interface of the application, as well as listening (and optionally responding) to
-	// application events from iOS.
-	[Register ("AppDelegate")]
-	public partial class AppDelegate : UIApplicationDelegate
-	{
-		// class-level declarations
-		UIWindow window;
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        // class-level declarations
 
-		//
-		// This method is invoked when the application has loaded and is ready to run. In this
-		// method you should instantiate the window, load the UI into it and then make the window
-		// visible.
-		//
-		// You have 17 seconds to return from this method, or iOS will terminate your application.
-		//
-		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
-		{
-			// create a new window instance based on the screen size
-			window = new UIWindow (UIScreen.MainScreen.Bounds);
+        public override UIWindow Window
+        {
+            get;
+            set;
+        }
 
-			var myViewController = new MyViewController();
-			// If you have defined a root view controller, set it here:
-			window.RootViewController = myViewController;
-			
-			// make the window visible
-			window.MakeKeyAndVisible ();
-			
-			return true;
-		}
-	}
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+        {
+            return true;
+        }
+
+        // This method is invoked when the application is about to move from active to inactive state.
+        // OpenGL applications should use this method to pause.
+        public override void OnResignActivation(UIApplication application)
+        {
+        }
+
+        // This method should be used to release shared resources and it should store the application state.
+        // If your application supports background exection this method is called instead of WillTerminate
+        // when the user quits.
+        public override void DidEnterBackground(UIApplication application)
+        {
+        }
+
+        /// This method is called as part of the transiton from background to active state.
+        public override void WillEnterForeground(UIApplication application)
+        {
+        }
+
+        /// This method is called when the application is about to terminate. Save data, if needed. 
+        public override void WillTerminate(UIApplication application)
+        {
+        }
+    }
 }
-
